5eb9db1824f4b25e4a75c837a7b77c8ac341b863
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-04-13 09:06:27 -0500

    added fabfile for fast deployment

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import run, cd, env
+
+env.user  = 'pi'
+env.hosts = ['raspberrypi.lan']
+
+def deploy():
+    web_dir = '/home/pi/Web'
+    with cd(web_dir):
+        run("git pull")
+        run("bower install")
