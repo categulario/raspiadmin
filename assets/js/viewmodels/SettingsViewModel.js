87bd0d0868ac0766b38ca65ad5df4c2c9dbd24c1
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-04-22 21:18:36 -0500

    load settings!

--- assets/js/viewmodels/SettingsViewModel.js
+++ assets/js/viewmodels/SettingsViewModel.js
@@ -1,14 +1,46 @@
+function SettingGroup(options, settings) {
+	var self = this;
+
+	self.title = ko.observable(options.title);
+	self.settings = ko.observableArray(options.settings.map(function (setting) {
+		return new Setting(setting, settings);
+	}));
+};
+
+function Setting(options, settings) {
+	var self = this;
+
+	self.template    = ko.observable(options.type+'-template');
+	self.key         = ko.observable(options.key);
+	self.humanKey    = ko.observable(options.key.capitalize());
+	self.description = ko.observable(options.description);
+	self.enabled     = ko.observable(options.key in settings);
+	self.value       = ko.observable(settings[options.key] || options.default);
+}
+
 function SettingsViewModel() {
 	var self = this;
 
 	self.template = ko.observable('settings-template');
 	self.title    = ko.observable('Settings');
 
+	self.settings = ko.observableArray([]);
+
 	self.load = function (done) {
 		$.get({
 			url: '/api/settings',
-			success: function (data) {
-				console.log(data);
+			success: function (settings) {
+				$.get({
+					url: '/assets/json/settings_model.json',
+					success: function (data) {
+						self.settings(data.map(function (group) {
+							return new SettingGroup(group, settings);
+						}));
+					},
+					error: function (xhr) {
+						alert('Something went wrong: '+xhr.status);
+					}
+				});
 			},
 			error: function (xhr) {
 				alert('Something went wrong: '+xhr.status);
